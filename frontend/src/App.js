13befb946843d61bf6d1f1ce1c9f6d6e36ec6b21
partelsandra <sandra.partel.001@voco.ee>
2023-12-20 15:08:51 +0200

    Update front end

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes
+import './App.css';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import NotesPage from './pages/NotesPage';
+import NoteDetail from './components/notes/NoteDetail';
+
+function App() {
+    return (
+        <Router>
+            <div className="App">
+                <Routes> {/* Use Routes */}
+                    <Route path="/login" element={<LoginPage />} /> {/* Use element prop */}
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/notes" element={<NotesPage />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+}
+
+export default App;
